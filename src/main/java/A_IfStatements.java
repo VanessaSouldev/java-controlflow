7874e16c069fc98f2fe2e79679bda4ef2f2b02a5
VanessaSouldev <vanessasouldev@gmail.com>
2023-09-29 14:54:37 +1000

    opdracht voldaan

--- src/main/java/A_IfStatements.java
+++ src/main/java/A_IfStatements.java
@@ -6,9 +6,9 @@ public class A_IfStatements {
         // IF Statements 1
         //
 
-        int number = 2;
-        // implementeer de conditie van het if statement.
-        if (true) {
+        int number = 6;
+
+        if (number % 1 != 0) {
             System.out.println("Odd");
         } else {
             System.out.println("Even");
@@ -21,11 +21,13 @@ public class A_IfStatements {
 
         float totalAmount = 100.5f;
         float discount = 0f;
-        // voeg een if statement toe voor het kortingsysteem.
-        // als de klant voor meer dan 50 besteld, wordt er 5% korting gegeven.
-        // als de klant voor meer dan 100 besteld, wordt er 10% korting gegeven.
-        // anders krijgt de klant 1% korting
-        // maak je geen zorgen over afronding.
+        if (totalAmount > 100f) {
+            discount = 0.1f;
+        } else if (totalAmount > 50f) {
+            discount = 0.05f;
+        } else {
+            discount = 0.01f;
+        }
 
         float totalAmountIncludingVAT = (totalAmount - (totalAmount * discount)) * 1.22f;
         System.out.println("Te betalen: " + totalAmountIncludingVAT);
@@ -36,11 +38,17 @@ public class A_IfStatements {
         //
         int x = 8;
         int y = 10;
-        // Voeg een if statement toe zodat alleen het juiste statement hieronder wordt uitgevoerd. Het if statement moet controleren of x groter is dan y, of y groter is dan x.
 
+
+        if (x > y) {
             System.out.println(x + " > " + y);
-            System.out.println(y + " > " + x);
-            System.out.println("I can't choose... I think they are equal...");
 
+        } else if (y > x) {
+
+            System.out.println(y + " > " + x);
+
+        } else {
+            System.out.println("I can't choose... I think they are equal...");
+        }
     }
 }
--- src/main/java/D_SwitchStatements.java
+++ src/main/java/D_SwitchStatements.java
@@ -7,11 +7,15 @@ public class D_SwitchStatements {
     //
 
     // Voeg aan onderstaand switch statement de juiste cases toe. Vergeet ook niet op de juiste plekken een `break` toe te voegen.
-    int y = 1;
+    int y = 2;
     switch (y) {
       case 1:
         System.out.println("=one");
+        break;
+      case 2:
         System.out.println("=two");
+        break;
+      default:
         System.out.println("!=one && !=two");
     }
 
@@ -24,7 +28,14 @@ public class D_SwitchStatements {
     // PepsiCo maakt Pepsi,7Up en Mountain Dew
     // Implementeer een switch statement dat, gegeven het drankje, het merk print.
     // Bij geen resultaat print het statement "onbekend".
-    String drink = "Mountain Dew";
+    String drink = "Fanta";
+    switch (drink){
+      case "cola", "Fanta", "Sprite":
+        System.out.println("Coca-Cola");
+        break;
+      case "Pepsi", "7Up", "Mountain Dew":
+        System.out.println("PepsiCo");
+    }
 
   }
 }
